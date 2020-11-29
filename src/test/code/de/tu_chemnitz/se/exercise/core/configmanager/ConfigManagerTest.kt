b8a51ef834d2f6731145b7f1f287e8bd9349b9f4
xsoophx <so.surf@web.de>
2020-11-29 21:07:37 +0100

    :construction: :art:  Added example tests and modified file structure.

---
+++ src/test/code/de/tu_chemnitz/se/exercise/core/configmanager/ConfigManagerTest.kt
@@ -0,0 +1,42 @@
+package de.tu_chemnitz.se.exercise.core.configmanager
+
+import org.junit.jupiter.api.Test
+
+class ConfigManagerTest {
+  private val manager = ConfigManager()
+
+  @Test
+  fun `comparing db and file content should work`(){
+
+  }
+
+  @Test
+  fun `writing in file should work`(){
+
+  }
+
+  @Test
+  fun `reading out of file should work`(){
+
+  }
+
+  @Test
+  fun `assembling all database configs should work`(){
+
+  }
+
+  @Test
+  fun `invoking of get function of a config out of db by id should work`(){
+
+  }
+
+  @Test
+  fun `invoking of save function of a config into the db should work`(){
+
+  }
+
+  @Test
+  fun `setting config path should work`(){
+
+  }
+}
