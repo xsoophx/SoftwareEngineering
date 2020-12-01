dccf0ed1fdd60015d4e5e461b4fce030bc8e507e
GumiBear93 <florian.thuemer@gmx.de>
2020-12-01 18:48:54 +0100

    added ConfigManager File, initialized some function names

---
+++ src/main/code/de/tu_chemnitz/se/exercise/core/configmanager/ConfigManager.kt
@@ -0,0 +1,35 @@
+package de.tu_chemnitz.se.exercise.core
+
+import org.junit.jupiter.api.Test
+
+class ConfigManager {
+  val configFilePath: String
+
+  fun checkDBSimilarity(): Boolean {
+
+  }
+
+  fun writeFile(): Unit{
+
+  }
+
+  fun readFile() {
+
+  }
+
+  fun assembleAllDatabases() {
+
+  }
+
+  fun getConfig(id: Int) {
+
+  }
+
+  fun saveConfig(): Unit {
+
+  }
+
+  fun setConfigPath(path: String) {
+
+  }
+}
