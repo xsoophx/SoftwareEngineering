719d0e47eb2ce927676da538e16c7c5909df5a58
Oliver Winkler <de.oliver.winkler@gmail.com>
2020-12-05 21:16:50 +0100

    Kleinere Änderungen

--- src/main/code/de/tu_chemnitz/se/exercise/codecharts/CodeChartsStringHandler.kt
+++ src/main/code/de/tu_chemnitz/se/exercise/codecharts/CodeChartsStringHandler.kt
@@ -1,7 +1,6 @@
 package de.tu_chemnitz.se.exercise.codecharts
 
 class CodeChartsStringHandler {
-<<<<<<< HEAD
   private val generatedStrings = setOf<String>()
 
   /**
@@ -11,10 +10,5 @@ class CodeChartsStringHandler {
    * @return StringArray
    */
   fun generateStrings(Input: Int, AllowedChars: ByteArray) {}
-=======
-  private lateinit var generatedStrings: Set<String>
-  fun generateStrings(Input: Int) {}
-  private fun orderList() {}
-  fun isInList(InputString: String) {}
->>>>>>> f0ba02e31c70b68aff5c5ecee789dd1085eb960d
+  fun getStrings() {}
 }
