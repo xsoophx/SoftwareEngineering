48bc0c9929674eb2948241189ae62244abb1ad47
xsoophx <so.surf@web.de>
2020-12-09 23:08:57 +0100

    Solved more errors.

--- src/main/code/de/tuchemnitz/se/exercise/persist/collections/CodeChartsConfigCollection.kt
+++
@@ -1,9 +0,0 @@
-package de.tuchemnitz.se.exercise.persist.collections
-
-import com.mongodb.client.MongoDatabase
-import de.tuchemnitz.se.exercise.persist.configs.CodeChartsConfig
-import org.litote.kmongo.getCollection
-
-class CodeChartsConfigCollection(
-    private val db: MongoDatabase
-) : AbstractCollection<CodeChartsConfig>(collection = db.getCollection())
