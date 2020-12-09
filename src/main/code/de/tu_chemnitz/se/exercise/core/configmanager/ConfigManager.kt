7ef3b9e14b76e0625fcf731b251ffdbd43899e9f
xsoophx <so.surf@web.de>
2020-12-09 18:30:50 +0100

    Made reading and writing working.

--- src/main/code/de/tu_chemnitz/se/exercise/core/configmanager/ConfigManager.kt
+++ src/main/code/de/tu_chemnitz/se/exercise/core/configmanager/ConfigManager.kt
@@ -1,63 +1,63 @@
 package de.tu_chemnitz.se.exercise.core.configmanager
 
-import com.mongodb.client.MongoDatabase
-import de.tu_chemnitz.se.exercise.persist.collections.AbstractCollection
 import de.tu_chemnitz.se.exercise.persist.collections.CodeChartsConfigCollection
 import de.tu_chemnitz.se.exercise.persist.configs.CodeChartsConfig
 import org.litote.kmongo.KMongo
 import org.litote.kmongo.eq
-import org.litote.kmongo.getCollection
-import java.awt.GraphicsEnvironment
 import java.io.File
 import java.io.FileNotFoundException
+import java.io.InputStream
 
 class ConfigManager {
-  var configFilePath: String = ""
+    var configFilePath: String = ""
 
-  private val client = KMongo.createClient() //get com.mongodb.MongoClient new instance
-  private val database = client.getDatabase("test") //normal java driver usage
-  private val collection = CodeChartsConfigCollection(database)
+    private val client = KMongo.createClient() // get com.mongodb.MongoClient new instance
+    private val database = client.getDatabase("test") // normal java driver usage
+    private val collection = CodeChartsConfigCollection(database)
 
-  fun checkDBSimilarity(): Boolean {
-    return true
-  }
-
-  fun writeFile(path: String, content: String): Unit {
-    if (path === "") return
-    try {
-      File(path).writeBytes(content.toByteArray())
-    } catch (e: FileNotFoundException) {
-      println("Error found: File does not exist")
+    fun checkDBSimilarity(): Boolean {
+        return true
     }
-  }
 
-  fun readFile(path: String): String {
-    var content = ""
-    try {
-      content = File(path).readBytes().toString()
-    } catch (e: FileNotFoundException) {
-      println("Error found: File does not exist")
-    } /*finally {
+    fun writeFile(path: String, content: String) {
+        if (path.isEmpty())
+            try {
+                File(path).bufferedWriter().use { out -> out.write(content) }
+                // File(path).writeBytes(content.toByteArray())
+            } catch (e: FileNotFoundException) {
+                println("Error found: File does not exist")
+            }
+    }
+
+    fun readFile(path: String): String {
+        var content = ""
+        try {
+            val inputStream: InputStream = File(path).inputStream()
+
+            content = inputStream.bufferedReader().use { it.readText() }
+            // content = File(path).bufferedReader().toString()
+            // content = File(path).readBytes().toString()
+        } catch (e: FileNotFoundException) {
+            println("Error found: File does not exist")
+        } /*finally {
       return content
     }*/
-    return content
-  }
+        return content
+    }
 
-  fun assembleAllDatabases(): String {
-    val placeholder = collection.find(CodeChartsConfig::matrixViewTime eq 4)
+    fun assembleAllDatabases(): String {
+        val placeholder = collection.find(CodeChartsConfig::matrixViewTime eq 4)
 
-    return ""
-  }
+        return ""
+    }
 
-  fun getConfig(id: Int) {
+    fun getConfig(id: Int) {
+    }
 
-  }
+    fun saveConfig() {
+    }
 
-  fun saveConfig(): Unit {
-
-  }
-
-  fun setConfigPath(path: String) {
-    configFilePath = path
-  }
+    fun setConfigPath(path: String) {
+        configFilePath = path
+    }
 }
--- src/test/code/de/tu_chemnitz/se/exercise/core/configmanager/ConfigManagerTest.kt
+++ src/test/code/de/tu_chemnitz/se/exercise/core/configmanager/ConfigManagerTest.kt
@@ -3,75 +3,58 @@ package de.tu_chemnitz.se.exercise.core.configmanager
 import assertk.assertThat
 import assertk.assertions.isEqualTo
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 import org.litote.kmongo.KMongo
-import java.io.FileNotFoundException
 
 class ConfigManagerTest {
-  private val manager = ConfigManager()
-  private val client = KMongo.createClient() //get com.mongodb.MongoClient new instance
-  private val database = client.getDatabase("test") //normal java driver usage
+    private val manager = ConfigManager()
+    private val client = KMongo.createClient() // get com.mongodb.MongoClient new instance
+    private val database = client.getDatabase("test") // normal java driver usage
 
-  companion object {
-    val paths = mapOf<String, String>(
-      "TestContent1" to "C:/Users/Florian/Desktop/file.txt",
-      "TestContent2" to "E:/TUCcloud flthu/Uni/WS 20-21 SE/Projekt/ProjektTest/file.txt",
-      "TestContent3" to ""
-    )
-    val pathsinv = mapOf<String, String>(
-      "TestContent4" to "TestString"
-    )
-  }
-
-  @Test
-  fun `comparing db and file content should work`() { //file content empty, file different from DB, file like DB
-    manager.checkDBSimilarity()
-  }
-
-  @Test
-  fun `writing in file should work`() { //test valid path, invalid path, missing path
-    pathsinv.forEach { (Content, Path) ->
-      assertThrows<FileNotFoundException> {
-        manager.writeFile(Path, Content)
-      }
+    companion object {
+        val paths = listOf(
+            Triple("TestContent1", "C:/Users/sosur/Desktop/file.txt", "TestContent1"),
+            Triple("TestContent2", "E:/se/file.txt", "TestContent2"),
+            Triple("TestContent3", "", ""),
+            Triple("TestContent4", "TestString", "TestContent4")
+        )
     }
-    paths.forEach { (Content, Path) ->
-      manager.writeFile(Path, Content)
+
+    @Test
+    fun `comparing db and file content should work`() { // file content empty, file different from DB, file like DB
+        manager.checkDBSimilarity()
     }
-  }
 
-  @Test
-  fun `reading out of file should work`() {
-    pathsinv.forEach { (_, Path) ->
-      assertThrows<FileNotFoundException> {
-        manager.readFile(Path)
-      }
+    @Test
+    fun `writing in file should work`() { // test valid path, invalid path, missing path
+        paths.forEach { (content, path, _) ->
+            assertThat(manager.writeFile(path, content))
+        }
     }
-    paths.forEach { (_, Path) ->
-      manager.readFile(Path)
+
+    @Test
+    fun `reading out of file should work`() {
+        paths.forEach { (_, path, expected) ->
+            assertThat(manager.readFile(path)).isEqualTo(expected)
+        }
     }
-  }
 
-  @Test
-  fun `assembling all database configs should work`() { //read and combine all Tools' config files into single config file
-
-  }
-
-  @Test
-  fun `invoking of get function of a config out of db by id should work`() {
-
-  }
-
-  @Test
-  fun `invoking of save function of a config into the db should work`() {
-
-  }
-
-  @Test
-  fun `setting config path should work`() {
-    paths.forEach {
-      manager.setConfigPath(it.value)
-      assertThat(manager.configFilePath).isEqualTo(it.value)
+    @Test
+    fun `assembling all database configs should work`() { // read and combine all Tools' config files into single config file
     }
-  }
+
+    @Test
+    fun `invoking of get function of a config out of db by id should work`() {
+    }
+
+    @Test
+    fun `invoking of save function of a config into the db should work`() {
+    }
+
+    @Test
+    fun `setting config path should work`() {
+       /* paths.forEach {
+            manager.setConfigPath(it.value)
+            assertThat(manager.configFilePath).isEqualTo(it.value)
+        }*/
+    }
 }
