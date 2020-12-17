6b92471f9127b39a9e579ade3601e71b193064e8
xsoophx <so.surf@web.de>
2020-12-17 17:03:41 +0100

    Updated build gradle.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,88 +2,106 @@ import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  java
-  kotlin("jvm") version "1.4.10"
-  id("org.jetbrains.kotlin.plugin.serialization") version "1.4.10"
+    java
+    kotlin("jvm") version "1.4.10"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.4.10"
+    id("org.jlleitschuh.gradle.ktlint-idea") version "9.4.1"
+    id("com.diffplug.spotless") version "5.8.2"
+    id("org.openjfx.javafxplugin") version "0.0.9"
 }
 
-group = "de.tu_chemnitz"
+group = "de.tuchemnitz"
 version = "1.0-SNAPSHOT"
 
 repositories {
-  mavenCentral()
+    mavenCentral()
+    maven {
+        url = uri("https://plugins.gradle.org/m2/")
+        url = uri("https://oss.sonatype.org/content/repositories/snapshots")
+    }
+}
+
+javafx {
+    version = "15.0.1"
+    modules = listOf("javafx.controls", "javafx.graphics")
 }
 
 object Version {
-  const val JUNIT = "5.7.0"
-  const val KOTEST = "4.3.0"
-  const val KOTLINX_COROUTINES = "1.3.8"
-  const val KOTLINX_SERIALIZATION = "0.20.0"
-  const val LOGBACK = "1.2.3"
-  const val MOCKK = "1.10.2"
-  const val SLF4J = "1.7.30"
+    const val JUNIT = "5.7.0"
+    const val KOTEST = "4.3.0"
+    const val KOTLINX_COROUTINES = "1.3.8"
+    const val KOTLINX_SERIALIZATION = "1.0.1"
+    const val LOGBACK = "1.2.3"
+    const val MOCKK = "1.10.2"
+    const val SLF4J = "1.7.30"
+    const val KMONGO = "4.2.2"
+    const val ASSERTK = "0.23"
+    const val SPOTLESS = "5.8.2"
+    const val KTLINT = "9.4.1"
+    const val TORNADOFX = "2.0.0-SNAPSHOT"
 }
 
 dependencies {
-  implementation(kotlin("stdlib-jdk8"))
-  implementation(kotlin("reflect"))
-  testImplementation(kotlin("test-junit5"))
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("reflect"))
+    testImplementation(kotlin("test-junit5"))
 
-  testImplementation("org.junit.jupiter:junit-jupiter-api:${Version.JUNIT}")
-  testImplementation("org.junit.jupiter:junit-jupiter-params:${Version.JUNIT}")
-  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${Version.JUNIT}")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:${Version.JUNIT}")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:${Version.JUNIT}")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${Version.JUNIT}")
 
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${Version.KOTLINX_COROUTINES}")
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:${Version.KOTLINX_COROUTINES}")
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:${Version.KOTLINX_COROUTINES}")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${Version.KOTLINX_COROUTINES}")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:${Version.KOTLINX_COROUTINES}")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:${Version.KOTLINX_COROUTINES}")
 
-  implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:${Version.KOTLINX_SERIALIZATION}")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:${Version.KOTLINX_SERIALIZATION}")
 
-  implementation("org.slf4j:slf4j-api:${Version.SLF4J}")
-  runtimeOnly("ch.qos.logback:logback-classic:${Version.LOGBACK}")
+    implementation("org.slf4j:slf4j-api:${Version.SLF4J}")
+    runtimeOnly("ch.qos.logback:logback-classic:${Version.LOGBACK}")
 
-  testImplementation("io.mockk:mockk:${Version.MOCKK}")
+    testImplementation("io.mockk:mockk:${Version.MOCKK}")
 
-  testImplementation("io.kotest:kotest-assertions-core-jvm:${Version.KOTEST}")
-  testImplementation("io.kotest:kotest-property-jvm:${Version.KOTEST}")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:${Version.KOTEST}")
+    testImplementation("io.kotest:kotest-property-jvm:${Version.KOTEST}")
 
-  implementation("org.litote.kmongo:kmongo:4.2.2")
+    implementation("org.litote.kmongo:kmongo:${Version.KMONGO}")
 
-  implementation("com.willowtreeapps.assertk:assertk:0.23")
+    implementation("com.willowtreeapps.assertk:assertk:${Version.ASSERTK}")
+
+    implementation("no.tornado:tornadofx:${Version.TORNADOFX}")
 }
 
 project.sourceSets {
-  getByName("main") {
-    java.srcDirs("src/main/code")
-    withConvention(KotlinSourceSet::class) {
-      kotlin.srcDirs("src/main/code")
+    getByName("main") {
+        java.srcDirs("src/main/code")
+        withConvention(KotlinSourceSet::class) {
+            kotlin.srcDirs("src/main/code")
+        }
     }
-  }
 
-  getByName("test") {
-    java.srcDirs("src/test/code")
-    withConvention(KotlinSourceSet::class) {
-      kotlin.srcDirs("src/test/code")
+    getByName("test") {
+        java.srcDirs("src/test/code")
+        withConvention(KotlinSourceSet::class) {
+            kotlin.srcDirs("src/test/code")
+        }
     }
-  }
 }
 
 tasks {
-  "wrapper"(Wrapper::class) {
-    gradleVersion = "6.7"
-  }
-
-  withType<KotlinCompile> {
-    kotlinOptions {
-      jvmTarget = "1.8"
-      freeCompilerArgs = listOf(
-        "-Xjvm-default=enable"
-      )
+    "wrapper"(Wrapper::class) {
+        gradleVersion = "6.7"
     }
-  }
 
-  withType<Test> {
-    useJUnitPlatform()
-  }
+    withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "11"
+            freeCompilerArgs = listOf(
+                "-Xjvm-default=enable"
+            )
+        }
+    }
+
+    withType<Test> {
+        useJUnitPlatform()
+    }
 }
-
--- src/main/code/de/tuchemnitz/se/exercise/codecharts/CodeChartsTool.kt
+++ src/main/code/de/tuchemnitz/se/exercise/codecharts/CodeChartsTool.kt
@@ -1,6 +1,9 @@
 package de.tuchemnitz.se.exercise.codecharts
 
-class CodeChartsTool {
+import de.tuchemnitz.se.exercise.core.configmanager.ConfigManager
+
+class CodeChartsTool(private val configManager: ConfigManager) {
+    // val setting = configManager.settings()
     fun start() {}
     private fun saveData() {}
 }
