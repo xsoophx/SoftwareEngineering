2ec3c0909c3a7b2d79dbfb20b3e5c65eb413e892
xsoophx <so.surf@web.de>
2021-01-21 15:43:28 +0100

    Readded Coordinates Zoom Maps.

--- src/main/code/de/tuchemnitz/se/exercise/dataanalyzer/DataClientHelperClasses.kt
+++ src/main/code/de/tuchemnitz/se/exercise/dataanalyzer/DataClientHelperClasses.kt
@@ -2,9 +2,6 @@ package de.tuchemnitz.se.exercise.dataanalyzer
 
 import de.tuchemnitz.se.exercise.codecharts.Dimension
 
-/**
- * Holds the information for depicting data from the database, which was created by data of the tools.
- */
 data class Coordinates(
     val xStart: Double,
     val yStart: Double,
@@ -12,4 +9,9 @@ data class Coordinates(
     val yEnd: Double,
     val picturePath: String,
     val scaledImageSize: Dimension
+)
+
+data class CoordinatesZoomMaps(
+    val zoomPosition: javafx.geometry.Point2D,
+    val radius: Int
 )
